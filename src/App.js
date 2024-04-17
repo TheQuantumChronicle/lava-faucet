1944e15960b84de1721620272f4df2234477cef5
TheQuantumChronicle <plusoneprojectnft@gmail.com>
2024-04-17 21:09:07 +0200

    Added auto-refresh after connecting wallet

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import WithdrawFunds from './components/WithdrawFunds';
 import ContributeFunds from './components/ContributeFunds';
 import TipFaucet from './components/TipFaucet';
 import MatrixRain from './components/MatrixRain';
-import LoadingSpinner from './components/LoadingSpinner';
 import Background from './components/Background';
 import contractABI from './contractABI.json';
 
 function App() {
   const [account, setAccount] = useState(null);
   const [contract, setContract] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
   const [showMatrixRain, setShowMatrixRain] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [isStartHereHovered, setIsStartHereHovered] = useState(false);
@@ -40,7 +38,6 @@ function App() {
           const signer = provider.getSigner();
           const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
           setContract(contractInstance);
-          setIsLoading(false);
           const userAddress = await signer.getAddress();
           setAccount(userAddress);
         } catch (error) {
@@ -57,49 +54,30 @@ function App() {
   const handleConnect = async () => {
     try {
       await requestAccount();
+      // If account successfully connected, reload the page
+      window.location.reload();
     } catch (error) {
       console.error('Error connecting to MetaMask:', error);
       alert('Error connecting to MetaMask. Please try again.');
     }
   };
 
-  const ConnectOverlay = () => {
-    return (
-      <div className="connect-overlay">
-        <div className="connect-overlay-content">
-          <p>Please connect your MetaMask wallet to use the faucet.</p>
-          <button onClick={handleConnect} style={{ fontFamily: 'Roboto' }}>Connect Wallet</button>
-        </div>
-      </div>
-    );
-  };
-
   const handleTransaction = async (transactionFunction) => {
-    if (!contract) {
-      console.error("Contract instance not initialized");
-      return;
-    }
-  
-    // Check if MetaMask is installed
     if (!window.ethereum) {
       console.error('MetaMask or compatible wallet not detected');
       alert('Please install MetaMask or a compatible wallet to proceed.');
       return;
     }
   
-    // Check if user is connected
     if (!account) {
       console.error('MetaMask account not connected');
       alert('Please connect MetaMask to your wallet');
       return;
     }
   
-    setIsLoading(true);
-  
     try {
       const result = await transactionFunction();
       if (result) {
-        // Show loading spinner until the transaction is confirmed
         const receipt = await result.wait();
         if (receipt.status === 1) {
           setShowMatrixRain(true);
@@ -115,15 +93,12 @@ function App() {
     } catch (error) {
       console.error('Transaction failed:', error);
       alert('Transaction failed. Please try again.');
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
     <div className="App">
       <Background />
-      {account === null && <ConnectOverlay />}
       <header className="App-header">
         <h1 className="title">LAVA FAUCET</h1>
         <div
@@ -155,56 +130,46 @@ function App() {
             </div>
           </div>
         </div>
-        {isLoading ? (
-          <LoadingSpinner />
-        ) : (
+        <div className="connect-wallet-container">
+          <button onClick={handleConnect} className="connect-wallet-button" style={{ fontFamily: "'Roboto', monospace" }}>Connect Wallet</button>
+        </div>
+        {account && (
           <>
-            {account ? (
-              <>
-                {contract && (
-                  <FaucetBalance contract={contract} />
-                )}
-                <div className="button-container withdraw-button">
-                  <WithdrawFunds 
-                    contract={contract} 
-                    account={account} 
-                    isLoading={isLoading}
-                    handleTransaction={handleTransaction}
-                    setShowMatrixRain={setShowMatrixRain}
-                  />
-                </div>
-                <div className="button-container contribute-button">
-                  <ContributeFunds
-                    contract={contract}
-                    account={account}
-                    isLoading={isLoading}
-                    handleTransaction={handleTransaction}
-                    setShowMatrixRain={setShowMatrixRain}
-                  />
-                  <div className="info-container contribution-info" style={{ fontFamily: "'Roboto Mono', monospace", fontWeight: "bold" }}>
-                    <p>Add LAVA (testnet) to our faucet (potential airdrop rewards...).</p>
-                  </div>
-                </div>
-                <div className="button-container tip-button">
-                  <TipFaucet
-                    contract={contract}
-                    account={account}
-                    isLoading={isLoading}
-                    handleTransaction={handleTransaction}
-                    setShowMatrixRain={setShowMatrixRain}
-                  />
-                  <div className="info-container tip-info" style={{ fontFamily: "'Roboto Mono', monospace", fontWeight: "bold" }}>
-                    <p>Tip Mainnet ETH/Magma to support (higher potential airdrop rewards...).</p>
-                  </div>
-                </div>
-                {showMatrixRain && (
-                  <MatrixRain showSuccessMessage={showSuccessMessage} />
-                )}
-              </>
-            ) : (
-              <div className="info-message">
-                <h2>Please connect MetaMask to your wallet</h2>
+            {contract && (
+              <FaucetBalance contract={contract} />
+            )}
+            <div className="button-container withdraw-button">
+              <WithdrawFunds 
+                contract={contract} 
+                account={account} 
+                handleTransaction={handleTransaction}
+                setShowMatrixRain={setShowMatrixRain}
+              />
+            </div>
+            <div className="button-container contribute-button">
+              <ContributeFunds
+                contract={contract}
+                account={account}
+                handleTransaction={handleTransaction}
+                setShowMatrixRain={setShowMatrixRain}
+              />
+              <div className="info-container contribution-info" style={{ fontFamily: "'Roboto', monospace", fontWeight: "strong" }}>
+                <p>Add LAVA (testnet) to our faucet (potential airdrop rewards...).</p>
               </div>
+            </div>
+            <div className="button-container tip-button">
+              <TipFaucet
+                contract={contract}
+                account={account}
+                handleTransaction={handleTransaction}
+                setShowMatrixRain={setShowMatrixRain}
+              />
+              <div className="info-container tip-info" style={{ fontFamily: "'Roboto Mono', monospace", fontWeight: "bold" }}>
+                <p>Tip Mainnet ETH/Magma to support (higher potential airdrop rewards...).</p>
+              </div>
+            </div>
+            {showMatrixRain && (
+              <MatrixRain showSuccessMessage={showSuccessMessage} />
             )}
           </>
         )}
